10b0a73563c4ab7514b6d5546d4053fc064c51c3
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2022-08-10 12:09:45 -0300

    adicionais

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module BankRailsApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.assets.initialize_on_precompile = false
     config.generators do |g|
       g.template_engine :haml
     end
